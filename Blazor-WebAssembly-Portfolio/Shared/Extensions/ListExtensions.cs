7c0c442344e9ee9e65ebe063d2fc0a74f380d56b
Blobbyguy <sam.gillard44@gmail.com>
2023-06-20 18:46:24 +1000

    5 - Adding Mudblazor and creating a landing page

---
+++ Blazor-WebAssembly-Portfolio/Shared/Extensions/ListExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BlazorWebAssemblyPortfolio.Shared.Extensions
+{
+	public static class ListExtensions
+	{
+		public static T GetRandomElement<T>(this IList<T> values)
+		{
+			var random = new Random();
+			return values[random.Next(values.Count)];
+		}
+	}
+}
+
