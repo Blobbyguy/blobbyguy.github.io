9cf9645ee1dcb86acf461128854a4a8c0fcaa127
Blobbyguy <sam.gillard44@gmail.com>
2026-02-08 21:04:43 +1100

    Highlight current Blueshift role in experience timeline

--- Blazor-WebAssembly-Portfolio/Shared/Experience/Experience.razor.cs
+++ Blazor-WebAssembly-Portfolio/Shared/Experience/Experience.razor.cs
@@ -1,10 +1,22 @@
 ﻿using Microsoft.AspNetCore.Components;
+
 namespace BlazorWebAssemblyPortfolio.Shared.Experience
 {
 	public partial class Experience : ComponentBase
 	{
+		private const string CurrentEmployer = "Blueshift";
+
 		[Inject]
 		public ExperienceData config { get; set; } = new();
+
+		private static bool IsCurrentEmployment(ExperienceDataItem experienceItem)
+		{
+			if (experienceItem.Company.Equals(CurrentEmployer, StringComparison.OrdinalIgnoreCase))
+			{
+				return true;
+			}
+
+			return experienceItem.Date.Contains("Present", StringComparison.OrdinalIgnoreCase);
+		}
 	}
 }
-
